e43687e130805aa92af375e2aafef37c1a37c547
prathamkotian <kotianpratham9@gmail.com>
2024-01-17 09:14:09 +0530

    Two Sum II - Input Array Is Sorted

--- Neetcode Solutions/Arrays & Hashing/167.py
+++ Neetcode Solutions/Arrays & Hashing/167.py
@@ -12,3 +12,17 @@ def twoSum(nums,target):
 nums=[2,7,11,15]
 target = 9
 print(twoSum(nums,target))
+
+#-----------------------------------------------------
+# given input array is sorted and it can also be solved with two pointer approach
+def twoSum(nums,target):
+    left,right=0,len(nums)-1
+    while left <=right:
+        current_sum=nums[left]+nums[right]
+        if current_sum==target:
+            return [left+1,right+1]
+        elif current_sum<target:
+            left+=1
+        else:
+            right-=1
+    return [-1,-1]
