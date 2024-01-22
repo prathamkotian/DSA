c07cd9b7e01c6a27c0bafd69b397558711663b20
prathamkotian <kotianpratham9@gmail.com>
2024-01-22 23:51:32 +0530

    Container with most water

--- Neetcode Solutions/Two Pointer/011.py
+++ Neetcode Solutions/Two Pointer/011.py
@@ -14,6 +14,24 @@ class Solution:
             else:
                 r -= 1
         return area
+class Solution_2:
+    def maxArea(self, height: [int]) -> int:
+        n = len(height)
+        left, right = 0, n - 1
+        res = 0
+
+        while left < right:
+            if height[left] > height[right]:
+                res = max(res, height[right] * (right - left))
+                right -= 1
+
+            else:
+                res = max(res, height[left] * (right - left))
+                left += 1
+                
+
+        return res
 
 height = [1,8,6,2,5,4,8,3,7]
 print(Solution().maxArea(height))
+print(Solution_2().maxArea(height))
