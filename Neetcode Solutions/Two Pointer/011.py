d616191d97b47c9415f448bcbd7874d17e2f6a1d
prathamkotian <kotianpratham9@gmail.com>
2024-01-23 09:06:39 +0530

    Container with most water

--- Neetcode Solutions/Two Pointer/011.py
+++ Neetcode Solutions/Two Pointer/011.py
@@ -18,19 +18,22 @@ class Solution_2:
     def maxArea(self, height: [int]) -> int:
         n = len(height)
         left, right = 0, n - 1
-        res = 0
+        area = 0
 
         while left < right:
+            # if height of left is greater, then we will consider height of right*(right-left)
+            # and we keep the greater value as it is and decrement right pointer
             if height[left] > height[right]:
-                res = max(res, height[right] * (right - left))
+                area = max(area, height[right] * (right - left))
                 right -= 1
-
+            # if height  of right is greater, then height of left*(right-left)
+            # and we will increment left pointer    
             else:
-                res = max(res, height[left] * (right - left))
+                area = max(area, height[left] * (right - left))
                 left += 1
                 
 
-        return res
+        return area
 
 height = [1,8,6,2,5,4,8,3,7]
 print(Solution().maxArea(height))
