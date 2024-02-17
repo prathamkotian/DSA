2248fc3dce70b39c50ca79b55f39623137fff0e6
prathamkotian <kotianpratham9@gmail.com>
2024-02-17 23:47:55 +0530

    Valid Sudoko

--- Neetcode Solutions/Arrays & Hashing/036.py
+++ Neetcode Solutions/Arrays & Hashing/036.py
@@ -1,6 +1,6 @@
 # valid suduko
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
         for i in range(9):
             for j in range(9):
                 if board[i][j] != ".":
